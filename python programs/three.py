673dad5830a582fb46f486b4b563dabfd2356e79
pavan <pavan@gmail.com>
2021-03-23 14:31:56 +0530

    first

---
+++ python programs/three.py
@@ -0,0 +1,13 @@
+u1="pavan"
+p1="123"
+u=input("enter user name")
+if(u==u1):
+    p=input("enter password")
+    print("welcome  "+u1)
+    if(p==p1):
+        print(p1)
+    else:
+            print("wrong password")
+else:
+    print("wrong input")
+
---
+++ python programs/two.py
@@ -0,0 +1,15 @@
+u1="pavan"
+u2="srinu"
+u3="kumar"
+p1="123"
+u=input("enter user name")
+p=input("enter password")
+if(u==u1 and p==p1):
+    print("welcome  "+u1)
+elif(u==u2 and p==p1):
+    print("welcome  "+u2)
+elif(u==u3 and p==p1):
+    print("welcome  "+u3)
+else:
+    print("wrong input")
+
